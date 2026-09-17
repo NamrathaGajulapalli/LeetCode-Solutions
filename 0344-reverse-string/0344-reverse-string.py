216ebbf39daf601b6734dd01da24a3007ca23032
NamrathaGajulapalli <namrathagajulapalli@gmail.com>
2026-09-17 14:53:48 +0530

    Time: 3 ms (44.61%), Space: 23.4 MB (46.16%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,13 +4,15 @@ class Solution:
         # for i in range(len(s)//2):
         #         s[i],s[n]=s[n],s[i]
         #         n-=1
+
+
+
         l=0
         r=len(s)-1
         while l<r:
             s[l],s[r]=s[r],s[l]
             l+=1
-            r-=1
-        return s    
+            r-=1  
 
 
 
