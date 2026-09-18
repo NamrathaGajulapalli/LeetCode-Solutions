8c29324b19ac57b38a57137213578d9e5c253d2b
NamrathaGajulapalli <namrathagajulapalli@gmail.com>
2026-09-18 14:45:54 +0530

    Time: 0 ms (100%), Space: 19.4 MB (9.49%) - LeetHub

--- 3136-valid-word/3136-valid-word.py
+++ 3136-valid-word/3136-valid-word.py
@@ -1,17 +1,30 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-        v=False
-        c=False
-        f=0
-        if len(word)>=3:
-            for i in word:
-                if i.isalnum():
-                    f+=1
-                    if i in "aeiouAEIOU":
-                        v=True
-                    if i not in "aeiouAEIOU" and i.isalpha():
-                        c=True  
-        return v and c and f==len(word)                 
+        v=0
+        c=0
+        if len(word)<3:
+            return False
+        for i in word:
+            if i.isalpha():
+                if i in 'aeiouAEIOU':
+                    v+=1
+                else:
+                    c+=1
+            if not i.isalnum():
+                return False
+        return v>=1 and c>=1                       
+        # v=False
+        # c=False
+        # f=0
+        # if len(word)>=3:
+        #     for i in word:
+        #         if i.isalnum():
+        #             f+=1
+        #             if i in "aeiouAEIOU":
+        #                 v=True
+        #             if i not in "aeiouAEIOU" and i.isalpha():
+        #                 c=True  
+        # return v and c and f==len(word)                 
 
             
 
