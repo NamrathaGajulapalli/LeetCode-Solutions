29d75308b83e93dd5a768257f4195d2c3359d03a
NamrathaGajulapalli <namrathagajulapalli@gmail.com>
2026-09-20 15:39:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.55%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.py
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.py
@@ -1,11 +1,19 @@
 class Solution:
     def isPalindromic(self, s: str) -> bool:
         l=[]
+        
         for i in s:
             x=''
             x=(bin(ord(i)))[2:].zfill(8)
             for i in x:
                 l.append(i)
-        return l==l[::-1]   
+        x=len(l)-1  
+        f=True      
+        for i in range(len(l)//2):
+            if l[i]!=l[x]:
+                f=False
+            x-=1
+        return f        
+
 
         
